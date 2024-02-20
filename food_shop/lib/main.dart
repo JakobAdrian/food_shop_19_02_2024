9c8692309cd223bb0479fe5d56dca90f26fc5eb8
Jakob Adrian <Jakob28adrian@gmail.com>
2024-02-20 20:48:40 +0100

    bin müde

--- food_shop/lib/main.dart
+++ food_shop/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,68 +15,209 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 70),
-          child: Column(
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
             children: [
               const SearchBar(
-                
                 hintText: "Search keywords...",
                 leading: Icon(Icons.search),
               ),
-              ListView(
-                scrollDirection: Axis.vertical,
-                children: [
-                  Image.asset("assets/images/background.png"),
-                  const SizedBox(height: 10),
-                  Container(
-                    width: 70,
-                    height: 70,
-                    color: Colors.grey,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
+              const Padding(
+                padding: EdgeInsets.fromLTRB(85.0, 8.0, 0, 0),
+              ),
+              Expanded(
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  children: [
+                    Stack(
                       children: [
-                        Container(
-                          child:
-                          Image.asset("assets/images/background.png"),
-                        )
-                    
+                        Image.asset("assets/images/background.png"),
+                        const Positioned(
+                          top: 140,
+                          left: 38,
+                          child: Text("20 % off on your \nfirst purchase",
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 0, 0, 0),
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              )),
+                        ),
                       ],
                     ),
-                  ),
-                  Column(
-                    children: [
-                      const Text("Featured products"),
-                      const SizedBox(height: 100),
-                      Container(
-                        width: 70,
-                        height: 70,
-                        color: Colors.grey,
-                        child: ListView(
-                          scrollDirection: Axis.vertical,
+                    const SizedBox(height: 10),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(width: 10),
+                        Text(
+                          "Categories",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(width: 265),
+                        Icon(Icons.arrow_forward_ios),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: 10,
+                      height: 70,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          const SizedBox(width: 10),
+                          Stack(
+                            children: [
+                              const CircleAvatar(
+                                maxRadius: 36,
+                                backgroundColor:
+                                    Color.fromRGBO(230, 242, 234, 1),
+                              ),
+                              Positioned.fill(
+                                child: Image.asset("assets/images/Group.png"),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(width: 10),
+                          Stack(
+                            children: [
+                              const CircleAvatar(
+                                maxRadius: 36,
+                                backgroundColor:
+                                    Color.fromRGBO(255, 233, 229, 1),
+                              ),
+                              Positioned.fill(
+                                child: Image.asset("assets/images/Vector.png"),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(width: 10),
+                          Stack(
+                            children: [
+                              const CircleAvatar(
+                                maxRadius: 36,
+                                backgroundColor:
+                                    Color.fromRGBO(255, 246, 227, 1),
+                              ),
+                              Positioned.fill(
+                                child:
+                                    Image.asset("assets/images/Group 216.png"),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(width: 10),
+                          Stack(
+                            children: [
+                              const CircleAvatar(
+                                maxRadius: 36,
+                                backgroundColor:
+                                    Color.fromRGBO(243, 239, 250, 1),
+                              ),
+                              Positioned.fill(
+                                child:
+                                    Image.asset("assets/images/Vector-1.png"),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(width: 10),
+                          CircleAvatar(
+                            maxRadius: 36,
+                            backgroundColor:
+                                const Color.fromRGBO(220, 244, 245, 1),
+                            child: Image.asset(
+                              "assets/images/Group-1.png",
+                              width: 48,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          CircleAvatar(
+                            maxRadius: 36,
+                            backgroundColor:
+                                const Color.fromRGBO(255, 232, 242, 1),
+                            child: Image.asset(
+                              "assets/images/vacuum.png",
+                              width: 48,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Column(
+                      children: [
+                        const Row(
                           children: [
-                            Image.asset("assets/images/background.png"),
-                      
+                            SizedBox(width: 10),
+                            Text("Featured products",
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            SizedBox(width: 210),
+                            Icon(Icons.arrow_forward_ios),
                           ],
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                        const SizedBox(height: 10),
+                        ListView(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: 180,
+                                  height: 300,
+                                  decoration: const BoxDecoration(
+                                    color: Color.fromRGBO(255, 232, 242, 1),
+                                  ),
+                                  child: Image.asset(
+                                      "assets/images/peach-24 1.png"),
+                                ),
+                                const SizedBox(width: 10),
+                                Stack(
+                                  children: [
+                                    Container(
+                                      width: 180,
+                                      height: 300,
+                                      decoration: const BoxDecoration(
+                                        color: Color.fromRGBO(255, 232, 242, 1),
+                                      ),
+                                    ),
+                                    const Positioned(
+                                      top: 25,
+                                      left: 20,
+                                      child: CircleAvatar(
+                                        maxRadius: 70,
+                                        backgroundColor:
+                                            Color.fromRGBO(252, 255, 217, 1),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      top: 65,
+                                      left: 15,
+                                      child: Image.asset(
+                                          "assets/images/aocado-21.png",
+                                          width: 150),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(height: 80),
             ],
           ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.card_travel), label: "Orders"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.heart_broken), label: "Wishlist"),
-          ],
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.card_travel), label: "Orders"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.heart_broken), label: "Wishlist"),
+            ],
+          ),
         ),
       ),
     );
